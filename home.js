ce983faca5a3361e980322534e98e79bd4d712e4
DanielMigchels <danielm12@hotmail.nl>
2020-11-05 16:24:06 +0100

    Improved Movement

--- home.js
+++ home.js
@@ -1,84 +1,102 @@
 var canvas = document.getElementById("playarea");
 var ctx = canvas.getContext("2d");
+
 ctx.canvas.width = window.innerWidth - 10;
 ctx.canvas.height = window.innerHeight - 10;
 
 var player = document.getElementById("player");
 
-var playerLocationX = window.innerWidth / 2;
-var playerLocationY = window.innerHeight / 2;
+var playerImage = new Image();
+playerImage.src = "sprites/player.png"
 
-var MovementSpeed = 13;
+var playerLocationX = 800;
+var playerLocationY = 400;
 
-function Body_Clicked(args) {
+var playerMovementX = 0;
+var playerMovementY = 0;
 
-    
+var playerMovementSpeed = 13;
 
-    ctx.beginPath();
-    ctx.moveTo(playerLocationX + 15, playerLocationY + 10);
-    ctx.lineTo((args.clientX - 20) + Math.floor(Math.random() * 21), (args.clientY - 20) + Math.floor(Math.random() * 21));
-    ctx.strokeStyle = "#e67300";
-    ctx.lineWidth = 4;
-    ctx.stroke();
-
-    setTimeout(function () {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    }, 50);
-
-
-}
-
-function Body_KeyPressed(args) {
+function Body_KeyDown(args) {
 
     switch (args.which) {
         case 119: // W
         case 87:
-            if (playerLocationY > 20) {
-                playerLocationY -= MovementSpeed;
-                UpdatePlayerLocation();
-                console.log("Player MoveUp");
-            }
+            playerMovementY = -1;
             break;
         case 97:
         case 65: // A
-            if (playerLocationX > 20) {
-                playerLocationX -= MovementSpeed;
-                UpdatePlayerLocation();
-                console.log("Player MoveLeft");
-            }
-
+            playerMovementX = -1;
             break;
         case 115:
         case 83: // S
-            if (playerLocationY <= (window.innerHeight - 70)) {
-                playerLocationY += MovementSpeed;
-                UpdatePlayerLocation();
-                console.log("Player MoveDown");
-            }
+            playerMovementY = 1;
             break;
         case 100:
         case 68: // D
-            if (playerLocationX <= (window.innerWidth - 70)) {
-                playerLocationX += MovementSpeed;
-                UpdatePlayerLocation();
-                console.log("Player MoveRight");
-            }
-            break;
-        case 32: //space
-            // SHOOT
+            playerMovementX = 1
             break;
         default:
-            alert(args.which);
+            console.log(args.which);
             break;
     }
 
 }
 
-function UpdatePlayerLocation() {
 
-    player.style.top = playerLocationY + "px";
-    player.style.left = playerLocationX + "px";
-
+function Body_KeyUp(args) {
+    switch (args.which) {
+        case 119: // W
+        case 87:
+            playerMovementY = 0;
+            break;
+        case 97:
+        case 65: // A
+            playerMovementX = 0;
+            break;
+        case 115:
+        case 83: // S
+            playerMovementY = 0;
+            break;
+        case 100:
+        case 68: // D
+            playerMovementX = 0;
+            break;
+        default:
+            console.log(args.which);
+            break;
+    }
 }
 
-UpdatePlayerLocation();
+
+function Render() {
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    if (playerLocationY < 10 && playerMovementY == -1) {
+        playerMovementY = 0;
+    }
+
+    if (playerLocationY > 810 && playerMovementY == 1) {
+        playerMovementY = 0;
+    }
+
+    if (playerLocationX < 10 && playerMovementX == -1) {
+        playerMovementX = 0;
+    }
+
+    if (playerLocationX > 1820 && playerMovementX == 1) {
+        playerMovementX = 0;
+    }
+
+    playerLocationX = (playerMovementX * playerMovementSpeed) + playerLocationX;
+    playerLocationY = (playerMovementY * playerMovementSpeed) + playerLocationY;
+
+    ctx.drawImage(playerImage, playerLocationX, playerLocationY, 80, 80);
+
+    setTimeout(() => {
+        Render();
+    }, 33);
+}
+
+Render();
